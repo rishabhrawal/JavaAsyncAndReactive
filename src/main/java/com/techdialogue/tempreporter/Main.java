6aed58fa5a9037bf5cde4c719ff17bded62d4b84
rishabh.rawal <rishabh_rawal@hotmail.com>
2019-01-06 18:13:19 +0000

    added processor

--- src/main/java/com/techdialogue/tempreporter/Main.java
+++ src/main/java/com/techdialogue/tempreporter/Main.java
@@ -3,13 +3,23 @@ package com.techdialogue.tempreporter;
 import java.util.concurrent.Flow;
 
 public class Main {
+
     public static void main(String[] args) {
-        getTemperatures("New York").subscribe(new TempSubscriber());
+        //getTemperatures("New York").subscribe(new TempSubscriber());
+        getCelciusTemperatures("New York").subscribe(new TempSubscriber());
     }
 
     public static Flow.Publisher<TempInfo> getTemperatures(String town){
         return subscriber -> subscriber.onSubscribe(
                 new TempSubscription(subscriber, town)
         );
+    }
+
+    public static Flow.Publisher<TempInfo> getCelciusTemperatures(String town){
+        return subscriber -> {
+            TempProcessor processor =  new TempProcessor();
+            processor.subscribe(subscriber);
+            processor.onSubscribe( new TempSubscription(processor, town));
+        };
     }
 }
---
+++ src/main/java/com/techdialogue/tempreporter/TempProcessor.java
@@ -0,0 +1,34 @@
+package com.techdialogue.tempreporter;
+
+import java.util.concurrent.Flow;
+
+public class TempProcessor implements Flow.Processor<TempInfo,TempInfo> {
+
+    private Flow.Subscriber<? super TempInfo> subscriber;
+
+    @Override
+    public void subscribe(Flow.Subscriber<? super TempInfo> subscriber){
+        this.subscriber = subscriber;
+    }
+
+    //method with business logic
+    @Override
+    public void onNext(TempInfo temp){
+        subscriber.onNext(new TempInfo(temp.getTown(), (temp.getTemp()-32) * 5/9));
+    }
+
+    @Override
+    public void onSubscribe(Flow.Subscription subscription){
+        subscriber.onSubscribe(subscription);
+    }
+
+    @Override
+    public void onError(Throwable throwable){
+        subscriber.onError(throwable);
+    }
+
+    @Override
+    public void onComplete(){
+        subscriber.onComplete();
+    }
+}
