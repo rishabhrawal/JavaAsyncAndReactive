1a7037d84b4318baf0e6244924225e52eadcfd16
rishabh.rawal <rishabh.rawal@accenture.com>
2019-01-09 17:42:50 +0000

    updates

--- src/main/java/com/techdialogue/functional/README.md
+++ src/main/java/com/techdialogue/functional/README.md
@@ -9,3 +9,6 @@ Ideally No Exceptions in Functional programming
 InvokeDynamic ==> for dynamically typed languages on JVM
 Lambdas are invoked using them and no classes are created for Lambdas
 Don't use big lambdas create a separate method and use Method reference for invocation
+
+
+Unchecked.consumer()
--- src/main/java/com/techdialogue/reactive/stock_trader/Sample.java
+++ src/main/java/com/techdialogue/reactive/stock_trader/Sample.java
@@ -12,20 +12,28 @@ public class Sample {
         Observable<StockInfo> feed2 = StockServer.getFeed(symbols);
 
         feed
+                .retry()
+                //.onExceptionResumeNext(feed)
                 //.onErrorReturnItem(new StockInfo("Error",9999.99))
                 //.onErrorReturnItem(new StockInfo("AGAIN",55555.55))
-                .onErrorResumeNext(feed2)
-                .onErrorReturnItem(new StockInfo("AGAIN",55555.55))
+                //.onErrorResumeNext(feed2)
+                //.onErrorReturnItem(new StockInfo("AGAIN",55555.55))
                 //.filter(stockInfo -> stockInfo.value>500)
                 .map(stockInfo -> new StockInfo(stockInfo.ticker, stockInfo.value * 0.9))
                 .subscribe(stockInfo -> System.out.println(stockInfo),
+                        //err -> {},
                         err -> System.out.println("ERROR: " +  err),
                         ()->System.out.println("DONE!"));
 
 
-        //Only one error is enough to close the data channel
+        //Only one error or exception is enough to close the data channel
         //separate handlers for ever error but how many ???
         //is there a way to resume on the same data channel
+
+        //on Exception Resume with a new Observable
+
+        //retry strategies upon error on the same channel
+
 
     }
 }
